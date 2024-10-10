b919cf092173647649dcdd3325855f2d5aaede5d
jungeun <kje2066@naver.com>
2024-10-10 18:30:31 +0900

    fix : listenIps 수정

--- config.js
+++ config.js
@@ -66,8 +66,8 @@ module.exports = {
     webRtcTransport: {
       listenIps: [
         {
-          ip: '3.34.193.132',
-          announcedIp: 'www.linkodemedia.shop',
+          ip: '0.0.0.0',
+          announcedIp: '3.34.193.132',
         }
       ],
       maxIncomingBitrate: 1500000,
